c54692a602f6faf72f1a8cb90109e1b309859bda
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-15 22:54:19 +0530

    03-vars.sh

---
+++ 03-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PER1=Trump
+PER2=Musk
+
+echo "$PER1:: Hey Hi $PER2, How are you?"
+echo "$PER2:: Hello $PER1, I'm fine. how are you doing"
+echo "$PER1:: I am fine too. what's up?"
+echo "$PER2:: Nothing, just going to mars."
