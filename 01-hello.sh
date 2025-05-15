2ecf4b11e85dc63b6a22eaf9e1765fed689da92d
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-15 22:33:05 +0530

    im just printing

---
+++ 01-hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Hi Musk, How are you?"
+echo "Musk: Hello Trump, I'm fine. how are you doing"
+echo "Trump: I am fine too. what's up"
+echo "Musk: Nothing, just going to mars"
